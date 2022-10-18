88ff7ca45d1e9ae3dd59a23302c162cd3e7cabc7
soha <sa4022@naver.com>
2022-10-18 14:58:37 +0900

    기본 작업 완료

--- src/main/java/com/bailey/dailytodolist_server/service/RealtimeDatabaseService.java
+++ src/main/java/com/bailey/dailytodolist_server/service/RealtimeDatabaseService.java
@@ -2,7 +2,11 @@ package com.bailey.dailytodolist_server.service;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class RealtimeDatabaseService {
     private final FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference databaseReference = database.getReference();
